0236c9ac41f1f709837dce80f84d3e4b1d7ab6ca
gracianx <graciana.valles@gmail.com>
2021-02-21 19:34:07 -0700

    js and html files

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,74 @@
 //Create variables to store the quiz questions
-let questionArr = [{
-    question: "1+1 = "
-    ansOne: "3"
-    ansTwo: "5"
-    ansThree: "2"
-},
-{
-    question: "5+5 = "
-    ansOne: "0"
-    ansTwo: "10"
-    ansThree: "5"
-}
+let counterEl = document.getElementById("counter");
+
+let startBtnEl = document.getElementById("startBtn");
+
+let questionsEl = document.getElementById("questions");
+
+let scoreEl = document.getElementById("score");
+
+let answersEl = document.getElementById("answers");
+
+let startQuizEl = document.getElementById("beginHere")
+
+let quizEl = document.getElementById("quiz")
+
+let questions = [{
+    question: "What tag is used to define an interactive field where users can enter data?",
+    answers: ["<p>", "<input>", "<br>"],
+    correctAnswer: "<input>"
+},{
+    question: "How do you add space around content using CSS?",
+    answers: ["<padding>", "<space>", "<space-around>"],
+    correctAnswer: "<padding>"
+
+},{
+  question: "What statement ends a loop?",
+  answers: ["Conditional statement", "//STOP LOOP", "Close loop"],
+  correctAnswer:"Conditional statement"
+  
+}];
+
+for (i = 0; i < questions.length; i++);
+
 
 //Use mouse-click events to start the quiz
-// button when I click it (√)
-var button = document.getElementById("#btn")
-// ... toggles between the two (√)
-var viewView1 = false;
-var count = 0;
-button.addEventListener("click", function(){
-  var view1 = document.getElementById("#view-1")
-  var view2 = document.getElementById("#view-2")
-  // If viewView1 is true, display "#view-1"
-  if (viewView1 === true) {
-    // ... it makes box "#view-1" dissappear (√)
-    // display view-1
-    view1.style.display = "block"
-    // hide view-2
-    view2.style.display = "none"
-  } 
-  // Else, display "#view-1"
-  else {
-    // ... makes another box appear (√)
-    view2.style.display = "block"
-    view1.style.display = "none"
+
+startBtnEl.addEventListener("click", function countdown(minutes) {
+   let seconds = 75;
+  let mins = minutes
+  function tick() {
+      var counter = document.getElementById("counter");
+      var current_minutes = mins-1
+      seconds--;
+      counter.innerHTML = "Time and Score: " + String(seconds);
+      if( seconds > 0 ) {
+          setTimeout(tick, 1000);
+      } else {
+          if(mins > 1){
+              countdown(mins-1);           
+          }
+      }
   }
-  // Swap between the viewView1s
-  viewView1 = !viewView1
-  // ... and keeps track of the count (√)
-  count++
-  var counter = document.getElementById("counter")
-  counter.textContent = count
-})
-//Write for loops to cycle through quiz questions
+  tick();
+});
 
-//Use key-press events to receive user input in the form of answers to quiz questions
+document.addEventListener("click", function startQuiz() {
+  let startQuizEl = document.getElementById("beginHere");
+  document.getElementById("beginHere").hidden = true;
+  document.getElementById("quiz").hidden = false;
+}, false); 
 
-//Create a time limit for the game using time functions
-
-//Write conditional statements to determine wrong and right answers
-
-//Use client-side storage to store high scores
-
+  //Write for loops to cycle through quiz questions
+  let txt = "";
+questions.forEach(function(showQuestion) {
+  document.getElementById("questionEl").innerHTML = txt;
+       } ); 
+    
+  //Use key-press events to receive user input in the form of answers to quiz questions
+  
+  //Create a time limit for the game using time functions
+  
+  //Write conditional statements to determine wrong and right answers
+  
+  //Use client-side storage to store high scores
